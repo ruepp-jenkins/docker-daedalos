8001ce6b3adfbd29d502de6bc8943115c56b65e6
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2024-10-27 12:11:39 +0100

    fix path

--- scripts/daedalos.sh
+++ scripts/daedalos.sh
@@ -3,4 +3,4 @@ set -e
 echo "Preparing DeadalOS"
 
 echo "Switching node version to latest alpine"
-sed -i '0,/^FROM/{s/^FROM.*/FROM node:22-alpine/}' Dockerfile
+sed -i '0,/^FROM/{s/^FROM.*/FROM node:22-alpine/}' repo/Dockerfile
